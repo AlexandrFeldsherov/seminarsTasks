72ed098ae4281381fc16784ce4e4ac0055555e13
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-07-03 01:39:34 +0300

    add task010

---
+++ task010/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+int DellNumber(int num)
+{
+    num = num / 10 % 10;
+    return num;
+}
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number / 1000 == 0 ^ number / 100 != 0)
+{
+    Console.Write("Вы ввели не трехзначное число, попоробуйте еще раз: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+int secondDigitNumbe = Math.Abs(DellNumber(number));
+Console.Write($"{number}->{secondDigitNumbe}");
